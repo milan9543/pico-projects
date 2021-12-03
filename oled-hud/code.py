59c34e442b21e17592d762e4e07a2d233567006a
milan9543 <milan9543@gmail.com>
2021-12-03 20:19:45 +0100

    fix oled hud

--- oled-hud/code.py
+++ oled-hud/code.py
@@ -14,8 +14,11 @@
 # RX  - GP8         SDA - GP0
 # TX  - GP9         SCK - GP1
 
+DEBUG = True
+
 import board
 import busio
+import time
 # GPS
 import adafruit_gps
 # SSD1306
@@ -28,7 +31,7 @@ import terminalio
 # GPS setup
 uart = busio.UART(board.GP8, board.GP9, baudrate=9600, timeout=10)
 gps = adafruit_gps.GPS(uart, debug=False) 
-gps.send_command(b"PMTK314,0,1,0,1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0")
+gps.send_command(b'PMTK314,1,1,1,1,1,1,0,0,0,0,0,0,0,0,0,0,0,0,0')
 gps.send_command(b"PMTK220,500")
 
 # Display setup
@@ -61,25 +64,59 @@ text_area = label.Label(
 )
 text_group.append(text_area)
 
+def debug_log(gps):
+        print("=" * 40)  # Print a separator line.
+        print(
+            "Fix timestamp: {}/{}/{} {:02}:{:02}:{:02}".format(
+                gps.timestamp_utc.tm_mon,  # Grab parts of the time from the
+                gps.timestamp_utc.tm_mday,  # struct_time object that holds
+                gps.timestamp_utc.tm_year,  # the fix time.  Note you might
+                gps.timestamp_utc.tm_hour,  # not get all data like year, day,
+                gps.timestamp_utc.tm_min,  # month!
+                gps.timestamp_utc.tm_sec,
+            )
+        )
+        print("{} {}".format(gps.latitude, gps.longitude))
+        print("Fix quality: {}".format(gps.fix_quality))
+        if gps.satellites is not None:
+            print("# satellites: {}".format(gps.satellites))
+        if gps.altitude_m is not None:
+            print("Altitude: {} meters".format(gps.altitude_m))
+        if gps.speed_knots is not None:
+            print("Speed: {} km/h".format(gps.speed_knots * 1.85200))
+        if gps.track_angle_deg is not None:
+            print("Track angle: {} degrees".format(gps.track_angle_deg))
+        if gps.horizontal_dilution is not None:
+            print("Horizontal dilution: {}".format(gps.horizontal_dilution))
+        if gps.height_geoid is not None:
+            print("Height geoid: {} meters".format(gps.height_geoid))
 
+
+last_print = time.monotonic()
 
 while True:
     gps.update()
     
-    if not gps.has_fix:
-        print("Waiting for fix - {} satellite(s)".format(gps.satellites))
-        satellite_count_label.text = str(gps.satellites)
-        display.show(satellite_group)
-        continue
+    current = time.monotonic()
+    if current - last_print >= 0.5:
+        last_print = current
+        if not gps.has_fix:
+            print("Waiting for fix - {} satellite(s)".format(gps.satellites))
+            satellite_count_label.text = str(gps.satellites)
+            display.show(satellite_group)
+            continue
 
-    speed = 0
-    if gps.speed_knots is not None:
-        speed_kph = gps.speed_knots * 1.85200
-        if speed_kph > 5:
-            speed = round(speed_kph)
-        
-    text_area.text = str(speed)
-    display.show(text_group)
+        if DEBUG:
+            debug_log(gps)
+            
+        speed = 0
+        if gps.speed_knots is not None:
+            speed_kph = gps.speed_knots * 1.85200
+            if speed_kph > 5:
+                speed = round(speed_kph)
+            
+        text_area.text = str(speed)
+        display.show(text_group)
     
         
 
